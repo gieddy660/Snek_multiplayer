577c1a085d4d747fb7cc6410d5d989412ed034ae
gieddy660 <47477441+gieddy660@users.noreply.github.com>
2021-04-17 21:40:23 +0200

    first commit toward the new communication protocol

--- server.py
+++ server.py
@@ -231,27 +231,6 @@ class Server:
                 self.players = new_players
 
     async def dispatch(self, reader, writer):
-        """
-              | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
-        s  =  [           play_id_hash        |
-              | d ]
-        r  =  [ a |   n   |   d   ]
-        ns =  [   x   |   y   ]*
-        ds =  [   x   |   y   ]*
-
-        s = client message ................. (9 bytes)
-        r = server response message ........ (5 bytes)
-        ns = new blocks (to be drawn) ...... (4 bytes * n)
-        ds = old blocks (to be cleared) .... (4 bytes * d)
-
-        play_id_hash = hash that represents each player ... (8 bytes)
-        d = new direction ................................. (1 byte)
-        a = still alive or dead ........................... (1 byte)
-        n = number of new blocks .......................... (2 bytes)
-        d = number of deleted blocks ...................... (2 bytes)
-        x = x coordinate of the block ..................... (2 bytes)
-        y = y coordinate of the block ..................... (2 bytes)
-        """
         try:
             player_hash = await reader.readexactly(Server.PLAYER_HASH_SIZE)
             new_dir = await reader.readexactly(Server.DIR_MESSAGE_SIZE)
