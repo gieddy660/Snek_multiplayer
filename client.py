858a864890be6a0cb8c9455c94fdeecff8e4dd84
gieddy660 <47477441+gieddy660@users.noreply.github.com>
2021-05-20 22:48:37 +0200

    updated client to work better with aiosnek

--- client.py
+++ client.py
@@ -7,27 +7,26 @@ import aiosnek
 
 DIRECTION = {'u': b'\x01', 'l': b'\x02', 'd': b'\x03', 'r': b'\x04', 'lol': b'\x05'}
 
-A_MESSAGE_SIZE = 1
-N_MESSAGE_SIZE = D_MESSAGE_SIZE = 2
-X_MESSAGE_SIZE = Y_MESSAGE_SIZE = 2
 
-direction: str
-
-
-async def gather_keyboard():
-    global direction
+async def gather_keyboard(host, port, hash_id):
     direction = 'u'
     while True:
+        new_direction = 0
         if keyboard.is_pressed('w') and direction != 'd':
-            direction = 'u'
+            new_direction = 'u'
         if keyboard.is_pressed('a') and direction != 'r':
-            direction = 'l'
+            new_direction = 'l'
         if keyboard.is_pressed('s') and direction != 'u':
-            direction = 'd'
+            new_direction = 'd'
         if keyboard.is_pressed('d') and direction != 'l':
-            direction = 'r'
+            new_direction = 'r'
         if keyboard.is_pressed('u'):
-            direction = 'lol'
+            new_direction = 'lol'
+
+        if new_direction != 0 and new_direction != direction:
+            direction = new_direction
+            await aiosnek.set_dir(host, port, hash_id, DIRECTION[direction])
+
         await asyncio.sleep(0.003)
 
 
@@ -56,27 +55,22 @@ def draw_screen_partial(news, olds):  # TODO: improve
         print()
 
 
-async def handle_connection(host, port):
-    hash_id = await aiosnek.register(host, port)
-    await asyncio.sleep(0.01)
-
+async def handle_connection(host, port, hash_id):
     while 1:
-        await aiosnek.set_dir(host, port, hash_id, DIRECTION[direction])
-        await asyncio.sleep(0.01)
-
         alive, new_blocks, old_blocks = await aiosnek.get_current_blocks(host, port, hash_id)
 
         if not alive:
             sys.exit()
 
         draw_screen_whole(new_blocks)
+        await asyncio.sleep(0.01)
 
 
 async def main():
-    host = 'localhost'
-    port = 12345
+    host, port = 'localhost', 12345
     print(host, port)
-    await asyncio.gather(handle_connection(host, port), gather_keyboard())
+    hash_id = await aiosnek.register(host, port)
+    await asyncio.gather(handle_connection(host, port, hash_id), gather_keyboard(host, port, hash_id))
 
 if __name__ == '__main__':
     grid = [[' ' for x in range(20)] for y in range(20)]
